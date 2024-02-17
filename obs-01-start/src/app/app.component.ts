a55f5546e1eeee334fea1a39071387451699c45e
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-02-17 16:28:27 +0530

    observables code complete

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  showHello = false;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userService.activatedEmitter.subscribe((didActivate) => {
+      this.showHello = didActivate;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userService.activatedEmitter.unsubscribe();
+  }
 }
--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+import { Observable } from 'rxjs-compat';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe((count) =>
+    //   console.log(count)
+    // );
+    const customIntervalObservable = Observable.create(
+      (observer) => {
+        let count = 0;
+        if (count === 10) {
+          observer.complete(); // this is different from throwing a error
+        }
+        setInterval(() => {
+          observer.next(count);
+          count++;
+        }, 1000);
+      },
+      (error) => {
+        console.log(error); // throwing an error cancels the observer but does not complete it
+      },
+      () => {
+        //this is a on-complete callback
+      }
+    );
+
+    const observerableAfterPipe = customIntervalObservable.pipe(
+      filter((e: number) => e % 2 === 0),
+      map((e: number) => {
+        return `Round :- ${e + 1}`;
+      })
+    );
+    this.firstObsSubscription = observerableAfterPipe.subscribe(console.log);
   }
 
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
