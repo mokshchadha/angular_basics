b7a4c9430b611af6fef44f89b6fbbda45532fec7
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-02-17 21:06:45 +0530

    added route params

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+  user: { id: number; name: string };
+  parmasSubsricption: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params.id,
+      name: this.route.snapshot.params.name,
+    };
+    this.parmasSubsricption = this.route.params.subscribe((o) => {
+      this.user = {
+        id: o.id,
+        name: o.name,
+      };
+    });
   }
 
+  ngOnDestroy(): void {
+    this.parmasSubsricption.unsubscribe();
+  }
 }
