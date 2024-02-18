92822677d016329bcb6922fd17af349c10832abb
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-02-18 18:09:33 +0530

    form array and controls complete

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      gender: new FormControl('male'),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
+
+  onAddHobby() {
+    (<FormArray>this.signupForm.get('hobbies')).push(new FormControl(''));
+  }
+
+  getHobbies() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
 }
