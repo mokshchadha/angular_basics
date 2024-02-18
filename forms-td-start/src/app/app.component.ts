63965d51fd324fcb4247d447d5a6c2f3422f6ab3
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-02-18 14:50:42 +0530

    submit form complete

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -11,8 +11,24 @@ export class AppComponent {
 
   defaultQuestion: string = 'pet';
   answer: string = '';
+  genders = ['male', 'female', 'other'];
+  submitted: boolean = false;
+
+  user = {
+    userName: '',
+    email: '',
+    secretQuestion: '',
+    gender: '',
+    answer: '',
+  };
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupform.setValue({ // in this you have to provide the whole form
+    //   userData: {
+    //     userName: suggestedName,
+    //   },
+    // });
+    this.signupform.form.patchValue({ userData: { userName: suggestedName } });
   }
 
   // onSubmit(form: NgForm) {
@@ -20,6 +36,21 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupform);
+    console.log(this.signupform.value);
+    console.log({ data: this.user });
+    this.submitted = true;
+    const user = {
+      userName: '',
+      email: '',
+      secretQuestion: '',
+      gender: '',
+      answer: '',
+    };
+    user['userName'] = this.signupform.value.userData.userName;
+    user['email'] = this.signupform.value.userData.email;
+    user['secretQuestion'] = this.signupform.value.secret;
+    user['answer'] = this.signupform.value.questionAnswer;
+    user['gender'] = this.signupform.value.gender;
+    this.user = user;
   }
 }
